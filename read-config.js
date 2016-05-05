8d6427673aa4d692a3b0b5ae018cbafe9a419e14
Daniel Tidwell <danieltidwell@Daniels-Mini.att.net>
2016-05-05 08:33:59 -0500

    add configuration reader

---
+++ read-config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+var supportedConfigPath = 'config/config.json';
+
+module.exports = function(rootPath) {
+    var configPath = path.join(rootPath, supportedConfigPath); 
+    var configModule = require(configPath);
+    if(!configModule) { 
+        throw `No config.json file found at ${supportedConfigPath}`;
+    }
+    
+    var env = process.env.NODE_ENV || 'development';
+    var config = configModule[env];
+    
+    if(!config) { 
+        throw `No configuration found for environment ${env} in ${supportedConfigPath}`;
+    } else { 
+        return config;
+    }
+}
